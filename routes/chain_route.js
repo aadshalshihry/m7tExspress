d3bae046e0154a9668f63d27505734e8cba0717c
Roman Alshehri <aalshehri08@hotmail.com>
2019-10-22 09:36:47 +0300

    Change some of view to ejs

--- routes/chain_route.js
+++ routes/chain_route.js
@@ -5,10 +5,10 @@ module.exports = function (app, upload) {
   // app.get('/company/index', passportConfig.isAuthenticated, companyController.companyIndex);
   // app.get('/company/create', passportConfig.isAuthenticated, companyController.companyCreatePage);
   //
-  app.post('/company/store',
-    passportConfig.isAuthenticated,
-    upload.single('logo'),
-    chainController.store);
+  // app.post('/company/store',
+  //   passportConfig.isAuthenticated,
+  //   upload.single('logo'),
+  //   chainController.store);
 
   //  ------------------------ API ---------------------------------
 
